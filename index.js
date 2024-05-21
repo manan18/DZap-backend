cab4717c2de73dd8c7ece23ce984f6ec27c2a254
manan18 <lcb2021017@iiitl.ac.in>
2024-05-22 02:34:08 +0530

    one

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ const app = express();
 app.use(express.json());
 app.use(cors({ origin: "*" }));
 
+const server = http.createServer((req, res) => {
+  // Set the response header
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  // Write some text to the response
+  res.end('Welcome to my simple Node.js app!');
+});
+
 app.use("/api/v1/currency", currencyRouter);
 
 app.get("/healthcheck", (req, res) => {
